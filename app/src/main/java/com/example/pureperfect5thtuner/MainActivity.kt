63ca9e6bf4a29304445cc873cd049fb211223048
Filip Kačić <151817961+FilipKacic@users.noreply.github.com>
2024-06-24 14:40:20 +0200

    icon, main textview, activity_main.xml permission, ...

--- app/src/main/java/com/example/pureperfect5thtuner/MainActivity.kt
+++ app/src/main/java/com/example/pureperfect5thtuner/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pureperfect5thtuner
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var frequencyValue = 432.0
+        updateFrequency(frequencyValue)
+    }
+
+    private fun updateFrequency(frequency: Double) {
+        val textViewFrequency = findViewById<TextView>(R.id.textViewFrequency)
+        val frequencyText = getString(R.string.frequency_placeholder, frequency)
+        textViewFrequency.text = frequencyText
     }
 }
